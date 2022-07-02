57ed4391c1c4f9f9f7558153170bd543f6d15ce6
yogendrasinghrawat <git.yrawat@gmail.com>
2022-07-02 20:08:47 +0530

    added jwt authentication and updated routes

---
+++ api/serializers.py
@@ -0,0 +1,42 @@
+import email
+from rest_framework import serializers
+from .models import Appointment
+from django.contrib.auth.models import User
+
+# Appointment Serializer
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = '__all__'
+    
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(style={"input_type":"password"},write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password','confirm_password']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+
+    def save(self):
+        password = self.validated_data['password']
+        confirm_password = self.validated_data['confirm_password']
+        if password!=confirm_password:
+            raise serializers.ValidationError({"error":"password doesn't match"})
+
+
+        if User.objects.filter(email=self.validated_data['email']).exists():
+            raise serializers.ValidationError({'error':"Email already Exists"})
+
+        
+        account = User(email=self.validated_data['email'],username=self.validated_data['username'])
+        account.set_password(password)
+        account.save()
